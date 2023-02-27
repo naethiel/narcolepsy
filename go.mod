5fdaf132202c9f82fb893d3eeeac218d254ace58
Nicolas Missika <nmissika@synthesio.com>
2023-02-27 18:18:41 +0100

    moved to using net/http to parse requests from file

--- go.mod
+++ go.mod
@@ -9,12 +9,16 @@ require (
 )
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.3.6 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/inconshreveable/log15"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/exp/slices"
 )
 
 func main() {
@@ -65,22 +68,47 @@ func fetch(ctx *cli.Context) error {
 
 	log15.Debug("lines read", "lines", lines)
 
-	// format lines by removing trailing whitespace
-	for i, l := range lines {
-		lines[i] = strings.TrimRight(l, " ")
+	rawRequests := getRawRequests(lines)
+	log15.Debug("raw requests parsed", "reqs", rawRequests, "len", len(rawRequests))
+
+	var (
+		parsed []parsedRequest
+		keys   []string
+	)
+
+	// now parse each raw request to build parsed requests ready to shoot
+	for _, r := range rawRequests {
+		log15.Debug("parsing request", "key", r.Key)
+		p, err := parseRequest(r)
+
+		if err != nil {
+			log15.Error("failed parsing request", "key", r.Key, "err", err)
+			return fmt.Errorf("parsing request %s: %w", r.Key, err)
+		}
+
+		parsed = append(parsed, p)
+		keys = append(keys, p.Key)
 	}
 
-	// join into a single string with line breaks
-	text := strings.Join(lines, "\n")
-	tokens, err := lex(text)
-	if err != nil {
-		log15.Error("lexing", "error", err)
-		return fmt.Errorf("lexing file: %w", err)
-	}
+	var answer string
+	err = survey.AskOne(&survey.Select{
+		Message: "Choose a request",
+		Options: keys,
+	}, &answer)
 
-	log15.Debug("done lexing", "tokens", tokens)
-
+	log15.Debug("chosen request", "req", answer)
 	return nil
+}
+
+func parseRequest(raw rawRequest) (parsedRequest, error) {
+	content := bufio.NewReader(strings.NewReader(raw.Definition))
+	var err error
+	out := parsedRequest{
+		Key: raw.Key,
+	}
+	out.Definition, err = http.ReadRequest(content)
+
+	return out, err
 }
 
 func readLines(filePath string) ([]string, error) {
@@ -102,4 +130,104 @@ func readLines(filePath string) ([]string, error) {
 	}
 
 	return lines, nil
+}
+
+type rawRequest struct {
+	Key        string
+	Definition string
+}
+
+type parsedRequest struct {
+	Key        string
+	Definition *http.Request
+}
+
+func getRawRequests(lines []string) []rawRequest {
+	var (
+		builtRequests []rawRequest
+		current       rawRequest
+	)
+
+	for i := 0; i < len(lines); i++ {
+		l := lines[i]
+
+		if strings.HasPrefix(l, "// ") || strings.HasPrefix(l, "# ") {
+			// skip comment lines
+			continue
+		}
+
+		if strings.HasPrefix(l, "### ") {
+			// separator line means new request
+
+			// append previously built request to accumulated requests if there is one
+			if len(current.Definition) != 0 {
+				builtRequests = append(builtRequests, current)
+			}
+
+			// spawn a clean new one
+			current = rawRequest{}
+			current.Key = strings.TrimPrefix(l, "### ")
+			continue
+		}
+
+		current.Definition += l + "\n"
+	}
+
+	// push last built request in list
+	if len(current.Definition) != 0 {
+		builtRequests = append(builtRequests, current)
+	}
+
+	// sanitize trailing space on each request definition
+	for i, r := range builtRequests {
+		builtRequests[i].Definition = formatRawReq(r.Definition)
+	}
+
+	return builtRequests
+}
+
+func formatRawReq(raw string) string {
+	s := strings.TrimSpace(raw)
+
+	requestLine, rest, _ := strings.Cut(s, "\n")
+
+	f := strings.Fields(requestLine)
+	var method, uri, proto string
+
+	//if f has len 2 or 1, we have missing method and/or proto
+	switch len(f) {
+	case 2:
+		if slices.Contains(allowedMethods, f[0]) {
+			method, uri = f[0], f[1]
+		}
+	case 1:
+		uri = f[0]
+	case 3:
+		method, uri, proto = f[0], f[1], f[2]
+	}
+
+	if len(method) == 0 {
+		method = "GET"
+	}
+	if len(proto) == 0 {
+		proto = "HTTP/1.1"
+	}
+
+	newReqLine := fmt.Sprintf("%s %s %s", method, uri, proto)
+
+	newDef := fmt.Sprintf("%s\n%s\n", newReqLine, rest)
+
+	return newDef
+}
+
+var allowedMethods = []string{
+	"GET",
+	"HEAD",
+	"POST",
+	"PUT",
+	"DELETE",
+	"CONNECT",
+	"PATCH",
+	"OPTIONS",
+	"TRACE",
 }
